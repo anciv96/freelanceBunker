4b20695e3aff69dc7ab275807e8113abe66afae1
anciv96 <anciv96@gmail.com>
2024-10-30 09:29:21 +0500

    Changed selenium options 2

--- parsers/browser_manager.py
+++ parsers/browser_manager.py
@@ -34,7 +34,9 @@ class WebDriverManager:
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-extensions")
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument("--remote-debugging-port=9222")
 
         return chrome_options
